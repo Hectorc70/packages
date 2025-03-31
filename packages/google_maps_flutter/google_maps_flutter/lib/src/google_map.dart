7e7ffde457819a34e2ba4d81a073b95a2b8a6002
Hector G <55261340+Hectorc70@users.noreply.github.com>
2025-03-31 10:40:46 -0600

    merge update imperative with 2.5.3

--- packages/google_maps_flutter/google_maps_flutter/lib/src/google_map.dart
+++ packages/google_maps_flutter/google_maps_flutter/lib/src/google_map.dart
@@ -363,10 +363,11 @@ class _GoogleMapState extends State<GoogleMap> {
   void didUpdateWidget(GoogleMap oldWidget) {
     super.didUpdateWidget(oldWidget);
     _updateOptions();
-    _updateMarkers();
-    _updatePolygons();
-    _updatePolylines();
-    _updateCircles();
+    // MOD imperative map updates (commented the lines below)
+    // _updateMarkers();
+    // _updatePolygons();
+    // _updatePolylines();
+    // _updateCircles();
     _updateTileOverlays();
   }
 
